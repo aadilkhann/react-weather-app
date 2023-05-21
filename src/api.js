311593c43eb9f62ef717e78562680d9350604afb
Adil Khan <khan01970@gmail.com>
2023-05-21 21:56:01 +0530

    Full Project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a50d0b64fbmshcbfc1e01c693a54p135b2ejsn3a39c84cce83", //GeoDb rapid api
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "966441ddc22b80362d506ccc29185e7f"; //Openweather API
